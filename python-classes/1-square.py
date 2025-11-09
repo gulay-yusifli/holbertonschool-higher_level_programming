732eed83f4dacaeb17b4c82108b1241f66dec8e4
gulay-yusifli <yusifligulay06@gmail.com>
2025-11-09 18:19:25 +0000

    task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Square class with private size."""
+
+
 class Square:
-    """Class Square that defines a square object
-    """
+    """Class that defines a square by its size."""
+
     def __init__(self, size):
-        """Initialize method that stores the size of the square
+        """Initialize a new Square.
 
         Args:
-            param1 (int): size of the square
+            size: size of the square.
         """
         self.__size = size
